1d8193a667ad3637492937671a7378938b6339f2
Andrew Gallant <jamslam@gmail.com>
2013-09-24 21:40:35 -0400

    Tweak the example and provide some explanation.

--- blast/blast_test.go
+++ blast/blast_test.go
@@ -2,12 +2,17 @@ package blast
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/TuftsBCB/seq"
 )
 
-// ExampleBlast demonstrates a very simple protein BLAST search.
+// ExampleBlast demonstrates a very simple protein BLAST search. Note that
+// you'll need to change `dbPath` to your own local BLAST database. The one
+// I used in the example is a BLAST database containing all of the protein
+// sequences from each strain of yeast from http://www.yeastgenome.org.
 func ExampleBlast() {
+	dbPath := "/home/andrew/research/repeats/data/blast/amino"
 	sequence := seq.Sequence{
 		Name: "YAL001C",
 		Residues: []seq.Residue(`
@@ -34,9 +39,8 @@ func ExampleBlast() {
 	`),
 	}
 
-	// dbPath := "/data/bio/blast/prot/20130630/db"
-	dbPath := "/home/andrew/research/repeats/data/blast/amino"
 	blaster := NewBlastp([]seq.Sequence{sequence}, dbPath)
+	blaster.SetFlag("evalue", 0.1)
 
 	results, err := Blast(blaster)
 	if err != nil {
@@ -44,7 +48,8 @@ func ExampleBlast() {
 		return
 	}
 
-	fmt.Println(results.Iterations[0].Hits[0].Accession)
+	hit := results.Iterations[0].Hits[0].Def
+	fmt.Println(strings.Contains(strings.ToLower(hit), "tfc3"))
 	// Output:
-	// 148033
+	// true
 }
